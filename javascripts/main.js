328c83628e8404eb76cbe66fb1a968caf88d3e4e
unknown <nai3267@EW54384R99VBMR.dow.com>
2013-09-19 02:20:39 -0400

    input box

--- javascripts/main.js
+++ javascripts/main.js
@@ -18,7 +18,28 @@ $(document).ready(function(){
         }
       });
     });
-    	
+    
+  $("#imageLoader").click(function() {
+	console.log("you have clicked the button!");
+	var tag = $("#inputTag").val();
+	console.log("Input box had: " + tag);
+	$.getJSON( 'http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?', {
+    tags: tag,
+    tagmode: "any",
+    format: "json"
+  }, function( data ) {	    
+      var imagesArr = [];
+      $.each( data.items, function( i, item ) {
+	      imagesArr.push("<img src=" + item.media.m + ">" + "</img>");
+        if ( i === 3 ) {
+          return false;
+        }
+      });
+      console.log(imagesArr);
+      $("#images").html(imagesArr.join(""));
+    });  
+  }
+  );
 });
 
 
