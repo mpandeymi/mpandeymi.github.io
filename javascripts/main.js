774b39f71a690e42d3e9206eeea0f7faeb9b1a56
unknown <nai3267@EW54384R99VBMR.dow.com>
2013-09-19 01:23:00 -0400

    Learning json

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,3 +5,20 @@ $(document).ready(function(){
 	});
 });
 
+
+ var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+  $.getJSON( flickerAPI, {
+    tags: "mount rainier",
+    tagmode: "any",
+    format: "json"
+  })
+    .done(function( data ) {
+	    console.log(data);
+      $.each( data.items, function( i, item ) {
+        $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+        if ( i === 3 ) {
+          return false;
+        }
+      });
+    });
+
