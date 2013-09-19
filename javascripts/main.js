cf1068de446916e69d36ed4fbac42ff0c68b68dc
unknown <nai3267@EW54384R99VBMR.dow.com>
2013-09-19 02:02:39 -0400

    corrected

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,18 +1,15 @@
-console.log('changed again.');
+console.log('json ajax');
 $(document).ready(function(){
 	$("body").click(function(){
 		$("#project_title").text("Rahul has taken over");
 	});
-});
 
 
- var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-  $.getJSON( flickerAPI, {
-    tags: "mount rainier",
+  $.getJSON( 'http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?', {
+    tags: "cats",
     tagmode: "any",
     format: "json"
-  })
-    .done(function( data ) {
+  }, function( data ) {
 	    console.log(data);
       $.each( data.items, function( i, item ) {
         $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
@@ -21,4 +18,7 @@ $(document).ready(function(){
         }
       });
     });
+    	
+});
+
 
